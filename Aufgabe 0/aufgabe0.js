d3399c429ac22e22df2e5520c371b58901fa4e36
maz0o <44026446+maz0o@users.noreply.github.com>
2019-03-21 14:56:29 +0100

    Aufgabe bearbeitet

---
+++ Aufgabe 0/aufgabe0.js
@@ -0,0 +1,14 @@
+function welcomemessage() {
+    var person = prompt("Please enter your name");
+    if (person == null) {
+        console.log("User canceled input");
+    }
+    else {
+        outputusername(person);
+    }
+}
+function outputusername(person2) {
+    document.getElementById("username").innerHTML = "Hello " + person2 + " how are you today nice to meet you";
+    console.log("Hello " + person2 + " how are you today nice to meet you");
+}
+//# sourceMappingURL=aufgabe0.js.map
---
+++ Aufgabe 0/aufgabe0.ts
@@ -0,0 +1,13 @@
+
+function welcomemessage(): void {
+  var person: string = prompt("Please enter your name");
+  if (person == null) {
+    console.log("User canceled input")
+  } else {
+    outputusername(person)
+  }
+}
+
+function outputusername(person2: string): void {
+  document.getElementById("username").innerHTML = "Hello " + person2 + " how are you today nice to meet you"; console.log("Hello " + person2 + " how are you today nice to meet you")
+}

873f60df6f2c7271c001b3b9e396ef75121180a8
maz0o <44026446+maz0o@users.noreply.github.com>
2019-05-04 16:56:12 +0200

    Add files via upload

---
+++ Aufgabe6/Code/data2.js
@@ -0,0 +1,9 @@
+var Aufgabe5;
+(function (Aufgabe5) {
+    Aufgabe5.icePicker = {
+        "stepper": [{ name: "Matcha", price: 1.5, amount: 0 }, { name: "Thunder", price: 1.5, amount: 0 }, { name: "Melon", price: 1.5, amount: 0 }, { name: "Fudge", price: 1.5, amount: 0 }, { name: "Grannys Apple Pie", price: 1.5, amount: 0 }, { name: "Cinnamon Heaven", price: 1.5, amount: 0 }],
+        "checkbox": [{ name: "Smarties", price: 0.20, amount: 0 }, { name: "Cottoncandy", price: 0.80, amount: 0 }, { name: "Brownies", price: 1, amount: 0 }, { name: "Cookie Dough", price: 1.2, amount: 0 }, { name: "Caramel", price: 0.6, amount: 0 }, { name: "Sprinkles", price: 0.30, amount: 0 }, { name: "Peanut Butter Cups", price: 1.5, amount: 0 }, { name: "Chocolate Pretzels", price: 0.8, amount: 0 }],
+        "select": [{ name: "cone", price: 0.20, amount: 0 }, { name: "cup", price: 0.20, amount: 0 }],
+    };
+})(Aufgabe5 || (Aufgabe5 = {}));
+//# sourceMappingURL=data2.js.map

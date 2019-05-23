5331aa770a5fc205f02d24533c1fab3d05f34f83
maz0o <44026446+maz0o@users.noreply.github.com>
2019-05-23 19:00:51 +0200

    upload task 8 to work with

---
+++ Aufgabe8/code/Client/DBClient.ts
@@ -0,0 +1,52 @@
+namespace DBClient {
+    window.addEventListener("load", init);
+    let serverAddress: string = "http://localhost:8100";
+    //let serverAddress: string = "https://<your>.herokuapp.com/";
+
+    function init(_event: Event): void {
+        console.log("Init");
+        let insertButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("insert");
+        let refreshButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("refresh");
+        insertButton.addEventListener("click", insert);
+        refreshButton.addEventListener("click", refresh);
+    }
+
+    function insert(_event: Event): void { /* geht inputelemente durch und baut mit einem command die eingegebenen Daten an den query String */
+        let inputs: HTMLCollectionOf<HTMLInputElement> = document.getElementsByTagName("input");
+        let query: string = "command=insert";
+        query += "&name=" + inputs[0].value;
+        query += "&firstname=" + inputs[1].value;
+        query += "&matrikel=" + inputs[2].value;
+        console.log(query);
+        sendRequest(query, handleInsertResponse);
+    }
+
+    function refresh(_event: Event): void {
+        let query: string = "command=refresh";
+        sendRequest(query, handleFindResponse);
+    }
+
+    function sendRequest(_query: string, _callback: EventListener): void { /* Sendet die Anfrage mit dem command refresh */
+        let xhr: XMLHttpRequest = new XMLHttpRequest();
+        xhr.open("GET", serverAddress + "?" + _query, true);
+        xhr.addEventListener("readystatechange", _callback);
+        xhr.send();
+    }
+
+    function handleInsertResponse(_event: ProgressEvent): void {
+        let xhr: XMLHttpRequest = (<XMLHttpRequest>_event.target);
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            alert(xhr.response);
+        }
+    }
+
+    function handleFindResponse(_event: ProgressEvent): void {
+        let xhr: XMLHttpRequest = (<XMLHttpRequest>_event.target);
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            let output: HTMLTextAreaElement = document.getElementsByTagName("textarea")[0];
+            output.value = xhr.response;
+            let responseAsJson: JSON = JSON.parse(xhr.response);
+            console.log(responseAsJson);
+        }
+    }
+}
